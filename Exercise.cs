dc320155fbb4890c22cd1790f2e11b8987e64dff
Dan Wiseman <dan@pennyfarthingapps.com>
2012-11-02 12:30:14 -0500

    added sqlite and classes for the data

---
+++ Exercise.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Data;
+using SQLite;
+
+namespace onermlog
+{
+	public class Exercise
+	{
+		[PrimaryKey, AutoIncrement]
+		public int ID { get; set; }
+		
+		public string Name { get; set; }
+		
+
+	}
+
+	// Each Log belongs to an Exercise via Exercise ID
+	public class Log
+	{
+
+		[PrimaryKey, AutoIncrement]
+		public int ID { get; set; }
+		
+		public int ExerciseID { get; set; }
+
+		public DateTime DateLogged { get; set; }
+
+		public double Weight { get; set; }
+	}
+}
+
