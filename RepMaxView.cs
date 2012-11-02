1642daf156fc36a9cfdfb4953a73f4fd6195ddc2
Dan Wiseman <dan@pennyfarthingapps.com>
2012-11-02 12:26:05 -0500

    finished basic layout. added dvc to UIView

--- RepMaxView.cs
+++ RepMaxView.cs
@@ -1,7 +1,7 @@
-
 using System;
 using System.Drawing;
 
+using MonoTouch.Dialog;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
@@ -9,9 +9,20 @@ namespace onermlog
 {
 	public partial class RepMaxView : UIViewController
 	{
+		DialogViewController _dvc;
+		RootElement _logRoot;
+
 		public RepMaxView () : base ("RepMaxView", null)
 		{
+			this._logRoot = new RootElement("Records");
+			this._dvc = new DialogViewController(UITableViewStyle.Plain, this._logRoot, false);
 
+			// sample data for testing
+			Section logSect = new Section();
+			StringElement recordString = new StringElement("200 lb.");
+			logSect.Add(recordString);
+
+			this._logRoot.Add(logSect);
 		}
 		
 		public override void DidReceiveMemoryWarning ()
@@ -21,6 +32,7 @@ namespace onermlog
 			
 			// Release any cached data, images, etc that aren't in use.
 		}
+
 		
 		public override void ViewDidLoad ()
 		{
@@ -28,6 +40,14 @@ namespace onermlog
 			
 			// Perform any additional setup after loading the view, typically from a nib.
 			this.View.BackgroundColor = UIColor.FromPatternImage(UIImage.FromBundle("images/white_carbon"));
+
+			// Bar
+			this.NavigationController.NavigationBar.SetBackgroundImage (UIImage.FromBundle ("images/navBar"), UIBarMetrics.Default);
+
+			// Put the dialog view controller into the UIView
+			this.dvcView.AddSubview(this._dvc.View);
+
+
 
 		}
 		
